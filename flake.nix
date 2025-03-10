f82974a2e8b4a7c9346bb176d9654c8cecd19b0e
Tomas Krbec <t.krbec@gmail.com>
2025-03-10 16:30:22 +0100

    multiple arch support

--- flake.nix
+++ flake.nix
@@ -1,17 +1,22 @@
-# flake.nix
 {
   description = "Standalone Couchbase Admin Tools overlay for Nixpkgs";
 
-  outputs = { self, nixpkgs, ... }:
-    let
-      supportedSystems = [ "x86_64-linux" ];
-    in
-    {
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-        overlays = [
-          couchbase-overlay.overlays.default
-        ];
-      };
-    }
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    # Make sure the couchbase overlay is available either as an input or via another mechanism.
+    couchbase-overlay.url = "path:./overlays/couchbase";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, couchbase-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem [ "x86_64-linux" "aarch64-darwin" ] (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ couchbase-overlay.overlays.default ];
+        };
+      in {
+        packages.default = pkgs;
+      }
+    );
 }
