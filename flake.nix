be5cc172383b88f7c18930f83dbe71cc2f0a50a9
Tomas Krbec <t.krbec@gmail.com>
2025-03-10 17:00:20 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -4,18 +4,18 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
-    # Make sure the couchbase overlay is available either as an input or via another mechanism.
-    couchbase-overlay.url = "path:./overlays/couchbase.nix";
   };
 
-  outputs = { self, nixpkgs, flake-utils, couchbase-overlay, ... }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem [ "x86_64-linux" "aarch64-darwin" ] (system:
       let
+        overlay = import ./overlays/couchbase.nix;
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ couchbase-overlay.overlays.default ];
+          overlays = [ overlay ];
         };
       in {
+        overlays.default = overlay;
         packages.default = pkgs;
       }
     );
