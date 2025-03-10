46ea156c8291835065672a20c27cd507fe09a4d9
Tomas Krbec <t.krbec@gmail.com>
2025-03-10 16:31:20 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
     # Make sure the couchbase overlay is available either as an input or via another mechanism.
-    couchbase-overlay.url = "path:./overlays/couchbase";
+    couchbase-overlay.url = "path:./overlays/couchbase.nix";
   };
 
   outputs = { self, nixpkgs, flake-utils, couchbase-overlay, ... }:
