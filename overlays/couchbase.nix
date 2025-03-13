e4b740dbb726761534bf9b455a30f511d7df4ba1
Tomas Krbec <mail@tomakrbec.cz>
2025-03-13 16:39:34 +0100

    cleaned
    Signed-off-by: Tomas Krbec <mail@tomakrbec.cz>

--- overlays/couchbase.nix
+++ overlays/couchbase.nix
@@ -20,7 +20,7 @@ self: super: {
         if sys == "x86_64-linux" then "0a3z8g5dnsdrfcfdrx0qfj2srr7n57kpljkn9f91408mmv30c282"
         else if sys == "aarch64-linux" then "00w9rpmrajsc8q001xm2z8qaa7q2kc782b3m669rcq1534rns0zq"
         else if sys == "x86_64-darwin" then "1jm5gars14w4983iag3pbp4p047vwbr9km7z12fwcl6h81ysi2v1"
-        else if sys == "aarch64-darwin" then "0vkm5w4x1a1b0qlaqki6zbazs3lpqf3lxh8i3lga0k7qy9i9jhm5"
+        else if sys == "aarch64-darwin" then "sha256-3f2heK4ecxf6p/MZoTiGi/E/4esOzS0Eyk349WN2+aw="
         else if sys == "x86_64-windows" then "0wwj38d3lqrj564kvfdxpr855kqv6c0fdccdfizy62a1dss3v3qs"
         else "";
     };
